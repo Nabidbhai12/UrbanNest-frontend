53b16cd8c8457af73c943ff9a777adb6e2d8faee
Nabidbhai12 <1905013@ugrad.cse.buet.ac.bd>
2024-03-02 20:43:23 +0600

    test

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://urban-nest-backend.vercel.app/:3000',
+        target: 'http://localhost:3000',
         secure: false,
       },
     },
